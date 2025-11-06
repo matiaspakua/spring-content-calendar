8d263715379a3aa5720db9a5ee5887e9dca4bd5d
matiaspakua <matiaspakua@gmail.com>
2025-11-06 19:53:24 +0100

    Adding changelog and two examples of class component and method bean

--- src/main/java/com/matiaspakua/content_calendar/Application.java
+++ src/main/java/com/matiaspakua/content_calendar/Application.java
@@ -2,12 +2,22 @@ package com.matiaspakua.content_calendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        // showBeansInformation(context);
+    }
+
+    private static void showBeansInformation(ConfigurableApplicationContext context) {
+        int count = context.getBeanDefinitionCount();
+        System.out.println(count);
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+    }
 
 }
---
+++ src/main/java/com/matiaspakua/content_calendar/component_bean/Message.java
@@ -0,0 +1,11 @@
+package com.matiaspakua.content_calendar.component_bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Message {
+
+    public String getMessage(){
+        return "Hola Mundo!";
+    }
+}
