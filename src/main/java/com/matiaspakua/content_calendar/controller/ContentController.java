1014ab67d52bef1def0b950c03d3bf27d4318899
matiaspakua <matiaspakua@gmail.com>
2025-11-10 21:41:39 +0100

    Adding the Bruno test for API

--- src/main/java/com/matiaspakua/content_calendar/controller/ContentController.java
+++ src/main/java/com/matiaspakua/content_calendar/controller/ContentController.java
@@ -4,11 +4,13 @@ import com.matiaspakua.content_calendar.model.Content;
 import com.matiaspakua.content_calendar.repository.ContentCollectionRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
@@ -24,5 +26,11 @@ public class ContentController {
     @GetMapping
     public List<Content> findAll() {
         return this.contentCollectionRepository.findAll().orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No content found"));
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Content> findById(@PathVariable Integer id) {
+        return Optional.ofNullable(contentCollectionRepository.findById(id))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found"));
     }
 }
